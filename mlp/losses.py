0b4705d8c963bb3503bc975b57d01fdae0d262f8
Ahmed <85986573+abo7mied@users.noreply.github.com>
2026-07-26 01:32:33 +0300

    Refactor loss computation to use backend array module

--- mlp/losses.py
+++ mlp/losses.py
@@ -3,6 +3,7 @@
 # LINES INDICATING MY OWNERSHIP, ASSUME THE CODE IS MY INSTRUCTOR'S.
 
 import numpy as np
+from .backend import get_array_module
 
 def compute_loss(A, Y, loss_type="cross_entropy"):
     """
@@ -21,19 +22,21 @@ def compute_loss(A, Y, loss_type="cross_entropy"):
 
     # Compute loss from aL and y.
     ### START OF MY CODE ###
+    xp = get_array_module(A)
+    
     epsilon = 1e-8 # small constant to avoid log(0) which can cause numerical instability
     if loss_type == "cross_entropy":
-        loss = (-1/m)*np.sum((Y * np.log(A + epsilon)) + (1-Y)*np.log(1-A + epsilon))
+        loss = (-1/m)*xp.sum((Y * xp.log(A + epsilon)) + (1-Y)*xp.log(1-A + epsilon))
     elif loss_type == "mse":
-        loss = (1/m)*np.sum((A - Y)**2)
+        loss = (1/m)*xp.sum((A - Y)**2)
     elif loss_type == "multiclass":
-        loss = (-1/m)*np.sum(Y * np.log(A + epsilon))
+        loss = (-1/m)*xp.sum(Y * xp.log(A + epsilon))
     else:
         raise ValueError("Unsupported loss_type: " + str(loss_type))
-    ### END OF MY CODE ###
     
-    loss = np.squeeze(loss)      # To make sure your loss's shape is what we expect (e.g. this turns [[17]] into 17).
+    loss = xp.squeeze(loss)      # To make sure your loss's shape is what we expect (e.g. this turns [[17]] into 17).
     assert(loss.shape == ())
+    ### END OF MY CODE ###
     
     return loss
 
